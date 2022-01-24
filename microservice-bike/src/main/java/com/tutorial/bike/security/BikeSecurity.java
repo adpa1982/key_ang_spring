8cbd408ca565c76d1257a7c535e76942e617e95c
adpa1982 <adpa363@hotmail.com>
2022-01-24 12:14:36 -0500

    Añaniendo interceptor keycloack

---
+++ microservice-bike/src/main/java/com/tutorial/bike/security/BikeSecurity.java
@@ -0,0 +1,20 @@
+package com.tutorial.bike.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class BikeSecurity extends WebSecurityConfigurerAdapter {
+	
+	protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().authenticated()
+                .and()
+                .oauth2ResourceServer().jwt();
+    }
+
+}
--- microservice-user/src/main/java/com/tutorial/user/service/UserService.java
+++ microservice-user/src/main/java/com/tutorial/user/service/UserService.java
@@ -5,6 +5,12 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -44,15 +50,19 @@ public class UserService {
     }
     
     public List<Car> getCars(Long userId) {
-        @SuppressWarnings("unchecked")
-		List<Car> cars = restTemplate.getForObject("http://car-service/car/byuser/" + userId, List.class);
-        return cars;
+        Jwt jwt = (Jwt) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Authorization", "Bearer " + jwt.getTokenValue());
+        ResponseEntity<List> cars = restTemplate.exchange("http://car-service/car/byuser/" + userId, HttpMethod.GET, new HttpEntity<>(httpHeaders), List.class);
+        return cars.getBody();
     }
 
-    public List<Bike> getBikes(Long userId) {
-    	@SuppressWarnings("unchecked")
-        List<Bike> bikes = restTemplate.getForObject("http://bike-service/bike/byuser/" + userId, List.class);
-        return bikes;
+    public List<Bike> getBikes(Long userId) {        
+        Jwt jwt = (Jwt) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Authorization", "Bearer " + jwt.getTokenValue());
+        ResponseEntity<List> bikes = restTemplate.exchange("http://bike-service/bike/byuser/" + userId, HttpMethod.GET, new HttpEntity<>(httpHeaders), List.class);
+        return bikes.getBody();
     }
     
     /*
